bbd3acd699ac7aa292300ac3da6519b086132448
Nsohan <sohan5538@gmail.com>
2026-09-14 09:25:43 +0600

    chore: bump versionCode to 2 and versionName to 0.0.02

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,8 +25,8 @@ android {
     applicationId = "com.nhs.customkeyboard"
     minSdk = 21
     targetSdk { version = release(36) }
-    versionCode = 1
-    versionName = "0.0.01"
+    versionCode = 2
+    versionName = "0.0.02"
   }
 
   sourceSets {
